c7d87f74595179f7b4255e56fa411e43106d21e7
akmhmgc <akimuhamaguchi@icloud.com>
2022-12-30 16:56:21 +0900

    fix: API側のエラーを引き継いで画面に出した

--- composables/useFetchCar.js
+++ composables/useFetchCar.js
@@ -3,8 +3,8 @@ export default async (id, filters) => {
 
   if (error.value) {
     throw createError({
-      ...error.value,
-      statusMessage: "Unable to fetch car",
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
     });
   }
   return { data };
--- composables/useFetchCars.js
+++ composables/useFetchCars.js
@@ -7,8 +7,8 @@ export default async (city, filters) => {
 
   if (error.value) {
     throw createError({
-      ...error.value,
-      statusMessage: "Unable to fetch cars",
+      statusCode: error.value.statusCode,
+      statusMessage: error.value.statusMessage,
     });
   }
   return { data, refresh };
