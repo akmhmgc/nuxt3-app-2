66a5f1e19e584672d590aef831afc944f952673e
akmhmgc <akimuhamaguchi@icloud.com>
2022-12-31 16:23:44 +0900

    feat: My Listing の削除機能の追加

--- pages/profile/listings/index.vue
+++ pages/profile/listings/index.vue
@@ -5,6 +5,13 @@ definePageMeta({
 });
 const user = useSupabaseUser()
 const { data: listings } = await useFetch(`/api/car/listings/user/${user.value.id}`)
+
+const handleDelete = async (id) => {
+  await $fetch(`/api/car/listings/${id}`, {
+    method: "delete"
+  });
+  listings.value = listings.value.filter((listing) => listing.id !== id)
+}
 </script>
 
 <template>
@@ -25,7 +32,7 @@ const { data: listings } = await useFetch(`/api/car/listings/user/${user.value.i
         ">+</NuxtLink>
     </div>
     <div class="shadow rounded p-3 mt-5">
-      <CarListingCard v-for="listing in listings" :key="listing.id" :listing="listing" />
+      <CarListingCard v-for="listing in listings" :key="listing.id" :listing="listing" @delete-click="handleDelete" />
     </div>
   </div>
 </template>
