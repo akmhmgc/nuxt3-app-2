c40ac68ce269e170a8e67c1224a6230d2170c4c7
akmhmgc <akimuhamaguchi@icloud.com>
2022-12-31 14:17:00 +0900

    feat: GET /api/cars/[city]

--- server/api/cars/[city].get.js
+++ server/api/cars/[city].get.js
@@ -1,26 +1,20 @@
-import cars from "@/data/cars.json";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 export default defineEventHandler((event) => {
   const { city } = event.context.params;
   const { make, minPrice, maxPrice } = getQuery(event);
 
-  let filterdCars = cars;
-  filterdCars = filterdCars.filter(
-    (car) => car.city.toLowerCase() === city.toLowerCase()
-  );
+  const filters = {
+    city: city.toLowerCase(),
+  };
 
-  if (make) {
-    filterdCars = filterdCars.filter(
-      (car) => car.make.toLowerCase() === make.toLowerCase()
-    );
-  }
+  if (make) filters.make = make;
+  if (minPrice || maxPrice) filters.price = {};
+  if (minPrice) filters.price.gte = parseInt(minPrice);
+  if (maxPrice) filters.price.lte = parseInt(maxPrice);
 
-  if (minPrice) {
-    filterdCars = filterdCars.filter((car) => car.price >= parseInt(minPrice));
-  }
-
-  if (maxPrice) {
-    filterdCars = filterdCars.filter((car) => car.price < parseInt(maxPrice));
-  }
-
-  return filterdCars;
+  return prisma.car.findMany({
+    where: filters,
+  });
 });
